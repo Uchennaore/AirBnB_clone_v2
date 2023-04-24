2c4f9b7b896199f8d69d5b1b2fee0284dd79fd6d
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-04-24 01:38:49 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
+"""Minimal flask app"""
+
 from flask import Flask, render_template
-"""
-intializing flask web app to listen on 0.0.0.0:5000
-"""
-from flask import Flask, render_template
-from models import storage, classes
+from models import storage
+from models import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/states_list')
-def states_list():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
-
-
-@app.route('/cities_by_states')
-def cities_by_states():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    """
+def closedb(foo):
+    """Closes db session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    """Route /cities_by_states"""
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
