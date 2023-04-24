b46e3197a12b5d4e854dc985e39cae8fc00b73dc
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-04-24 01:29:17 +0100

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
+"""Minimal flask app"""
+
 from flask import Flask
-"""
-intializing flask web app to listen on 0.0.0.0:5000
-"""
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_world():
-    """
-    Display "Hello HBNB!"
-    """
-    return ('Hello HBNB!')
+@app.route('/', strict_slashes=False)
+def index():
+    """Route index"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    route /hbnb displays
-    """
-    return ('HBNB')
+    """Route /hbnb"""
+    return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """
-    replaces _ with spaces
-    """
-    return ("C {}".format(text.replace('_', ' ')))
+    """Route /c"""
+    return "C %s" % text.replace("_", " ")
 
 
-@app.route('/python')
-@app.route('/python/<text>')
-def py(text="is cool"):
-    """
-    default adds is cool and replaces _ with space
-    """
-    return("Python {}".format(text.replace('_', ' ')))
+@app.route('/python',
+           defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """Route /python"""
+    return "Python %s" % text.replace("_", " ")
 
 
-@app.route('/number/<int:n>')
-def num(n):
-    """
-    displays only if the added is an int
-    """
-    return ("{:d} is a number".format(n))
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Route /number"""
+    return "%d is a number" % n
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
