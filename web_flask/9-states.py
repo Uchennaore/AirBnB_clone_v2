8eff45a62b47fd29dd6e8986ea694975ec793bab
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-04-24 01:40:23 +0100

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,51 +1,36 @@
 #!/usr/bin/python3
+"""Minimal flask app"""
+
 from flask import Flask, render_template
-"""
-intializing flask web app to listen on 0.0.0.0:5000
-"""
-from flask import Flask, render_template
-from models import storage, classes
+from models import storage
+from models import State
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/states')
-@app.route('/states_list')
-def states_list():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('7-states_list.html', states=states))
-
-
-@app.route('/states/<n>')
-def state_id(n):
-    """
-    routes /state/<id> to display state of given id
-    """
-    states = storage.all(classes["State"])
-    state_key = "State.{}".format(n)
-    if state_key in states:
-        state = states[state_key]
-    else:
-        state = None
-    return (render_template('9-states.html', state=state))
-
-
-@app.route('/cities_by_states')
-def cities_by_states():
-    """
-    """
-    states = storage.all(classes["State"]).values()
-    return (render_template('8-cities_by_states.html', states=states))
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    """
+def closedb(foo):
+    """Closes db session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/states', strict_slashes=False, defaults={'id': None})
+@app.route('/states/<id>', strict_slashes=False)
+def states(id):
+    """Route /states"""
+    state = states = None
+    if not id:
+        states = list(storage.all(State).values())
+    else:
+        states = storage.all(State)
+        key = "State." + id
+        if key in states:
+            state = states[key]
+        else:
+            state = None
+        states = []
+    return render_template('9-states.html', **locals())
+
+
+if __name__ == '__main__':
+    storage.reload()
+    app.run("0.0.0.0", 5000)
