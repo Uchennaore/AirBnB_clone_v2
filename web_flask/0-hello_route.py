76ac194a7d69697ab809746e07882d7636497985
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-04-27 02:59:59 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Minimal flask app"""
-
+"""Starts a Flask web application
+"""
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Route index"""
-    return "Hello HBNB!"
+    """Display 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+    app.run('0.0.0.0')
